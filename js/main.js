87813c947bf1bc9f672f500c6ab7f208efc33795
quandiqdev <quandiqdev@gmail.com>
2023-09-07 20:54:30 +0500

    first-commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// try {
+//   // Burger Menu
+//   const hamburger = document.querySelector(".hamburger");
+//   const menu = document.querySelector(".header__menu");
+//   // const menuLinks = document.querySelectorAll(".header__nav-link");
+
+//   hamburger.addEventListener("click", (e) => {
+//     hamburger.classList.toggle("hamburger--active");
+//     menu.classList.toggle("header__menu--active");
+
+//     if (hamburger.classList.contains("hamburger--active")) {
+//       document.body.classList.add("no-scroll");
+//     } else {
+//       document.body.classList.remove("no-scroll");
+//     }
+//   });
+// } catch (e) {}
