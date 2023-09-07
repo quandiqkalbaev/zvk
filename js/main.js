96b0b509fe7882ed327163533162f6c1bb6ace6f
quandiqdev <quandiqdev@gmail.com>
2023-09-08 03:29:48 +0500

    desktop version ready

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-// try {
-//   // Burger Menu
-//   const hamburger = document.querySelector(".hamburger");
-//   const menu = document.querySelector(".header__menu");
-//   // const menuLinks = document.querySelectorAll(".header__nav-link");
-
-//   hamburger.addEventListener("click", (e) => {
-//     hamburger.classList.toggle("hamburger--active");
-//     menu.classList.toggle("header__menu--active");
-
-//     if (hamburger.classList.contains("hamburger--active")) {
-//       document.body.classList.add("no-scroll");
-//     } else {
-//       document.body.classList.remove("no-scroll");
-//     }
-//   });
-// } catch (e) {}
+$('.product').slick({
+    slidesToShow:4,
+    slidesToScroll:1,
+    arrows:true,
+    dots:false,
+    rows:1,
+    responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    
+})
